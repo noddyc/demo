8c40831443b21eb1ba93ec84fb960a54432f59c0
jianhe2git <59978308+noddyc@users.noreply.github.com>
2024-03-22 20:27:25 -0400

    Initial commit

---
+++ PaydayDemo/src/main/java/com/example/payday/PaydayApplication.java
@@ -0,0 +1,13 @@
+package com.example.payday;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PaydayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PaydayApplication.class, args);
+    }
+
+}
---
+++ PaydayDemo/src/test/java/com/example/payday/PaydayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.payday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PaydayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
